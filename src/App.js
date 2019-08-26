10ece4ecaf4efa7d0a0df16ff0f189fadaf05d57
Joey <joey.scott@tedderindustries.com>
2019-08-26 13:41:14 -0700

    Initial project setup for CrowdHub Test App.  react-native init, npm i react-navigation && gesture-handler, also rn link gesture-handler to link depends

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
